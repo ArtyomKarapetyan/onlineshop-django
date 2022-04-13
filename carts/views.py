a35a36abdfe5709cd11919ee80957464c54964a8
ARTYOM <92950445+ArtyomKarapetyan@users.noreply.github.com>
2022-04-13 22:29:24 +0400

    2_commit

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from carts.models import Cart, CartItem
 from store.models import Product
@@ -14,6 +15,11 @@ def _cart_id(request):
 
 
 def add_cart(request, product_id):
+    color = request.GET['color']
+    size = request.GET['size']
+    return HttpResponse(color + ' ' + size)
+    exit()
+    
     product = Product.objects.get(id=product_id) #get the product
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request)) #get the cart using the cart_id present in the session
