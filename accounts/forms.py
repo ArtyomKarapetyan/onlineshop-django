e7dfc123930be76942c69ffd3d81e73bbfd3c826
ARTYOM <92950445+ArtyomKarapetyan@users.noreply.github.com>
2022-05-29 11:37:42 +0400

    dashboard

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,10 @@
 from django import forms
-from .models import Account
+from .models import Account, UserProfile
 
 class RegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'Enter password'
+        'placeholder': 'Enter password',
+        'class': 'form-control'
     }))
     confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder': 'Confirm password'
@@ -11,6 +12,16 @@ class RegistrationForm(forms.ModelForm):
     class Meta:
         model = Account
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+
+
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Password does not match!"
+            )
 
 
     def __init__(self, *args, **kwargs):
@@ -23,9 +34,24 @@ class RegistrationForm(forms.ModelForm):
             self.fields[i].widget.attrs['class'] = 'form-control'
 
     
-    def clean(self):
-        cleaned_data = super(RegistrationForm, self).clean()
-        password = cleaned_data.get('password')
-        confirm_password = cleaned_data.get('confirm_password')
-        if password != confirm_password:
-            raise forms.ValidationError("Password does not match!")
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = ['first_name', 'last_name', 'phone_number']
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        for i in self.fields:
+            self.fields[i].widget.attrs['class'] = 'form-control'
+
+
+
+class UserProfileForm(forms.ModelForm):
+    profile_picture = forms.ImageField(required=False, error_messages={'invalid': ('image files only')}, widget=forms.FileInput)
+    class Meta:
+        model = UserProfile
+        fields = ['address_line_1', 'address_line_2', 'profile_picture', 'city', 'state', 'country']
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        for i in self.fields:
+            self.fields[i].widget.attrs['class'] = 'form-control'
--- onlineshop/urls.py
+++ onlineshop/urls.py
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from xml.dom.minidom import Document
-from xml.etree.ElementInclude import include
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from onlineshop.settings import MEDIA_ROOT
+from .settings import MEDIA_ROOT
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
